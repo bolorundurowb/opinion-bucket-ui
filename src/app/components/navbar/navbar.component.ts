715759563f7543cd79a7dc953984fbd90d7102ff
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-02 23:35:27 +0100

    #17 | add bare navbar markup

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
 
+export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
   }
-
 }
