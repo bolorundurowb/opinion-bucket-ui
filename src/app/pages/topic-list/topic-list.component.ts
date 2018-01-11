e3349ea098f1abf088e3925e0fd31713fd021b65
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-11 15:18:23 +0100

    #15 | add the loading screen when the page loads

--- src/app/pages/topic-list/topic-list.component.ts
+++ src/app/pages/topic-list/topic-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-topic-list',
@@ -6,6 +6,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./topic-list.component.scss']
 })
 
-export class TopicListComponent {
+export class TopicListComponent implements OnInit {
+  isLoading: boolean
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+  }
 }
