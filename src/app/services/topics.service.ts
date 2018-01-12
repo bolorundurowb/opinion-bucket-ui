644347504d01aaa505994b67456410278e2a6778
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-12 11:49:24 +0100

    #15 | add the topics service to retrieve the topics

---
+++ src/app/services/topics.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
+
+@Injectable()
+export class TopicsService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}topics`);
+  }
+}
