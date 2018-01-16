f7c9c43dcd3e5154d9f1eff6ab6c5dd077c5f2c1
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-16 16:27:16 +0100

    #33 | remove unused imports

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 
 declare var jQuery: any;
