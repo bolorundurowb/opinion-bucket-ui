8e35d3e3ba00cdcc5224cb993710ccad720eb67e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-22 00:46:10 +0100

    #14 | add the topic details retrieval method to the service

--- src/app/pages/topic-details/topic-details.component.ts
+++ src/app/pages/topic-details/topic-details.component.ts
@@ -1,8 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-topic-details',
   templateUrl: './topic-details.component.html',
   styleUrls: ['./topic-details.component.scss']
 })
-export class TopicDetailsComponent{}
+export class TopicDetailsComponent implements OnInit {
+  topicId: string;
+
+  isLoading: boolean;
+  hasError: boolean;
+  errorMessage: string;
+
+  constructor (
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.topicId = params.id;
+    });
+
+    this.isLoading = true;
+  }
+}
