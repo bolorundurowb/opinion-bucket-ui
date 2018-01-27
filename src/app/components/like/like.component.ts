f8e4434ad0f48261a994a0353cb0c264a68dfa9b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-27 16:11:24 +0100

    #9 | add logic for checking displaying and enabling the button

--- src/app/components/like/like.component.ts
+++ src/app/components/like/like.component.ts
@@ -1,26 +1,59 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {OpinionsService} from '../../services/opinions.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-like',
   templateUrl: 'like.component.html',
   styleUrls: ['like.component.scss'],
-  providers: [OpinionsService]
+  providers: [OpinionsService, AuthService]
 })
-export class LikeComponent {
+export class LikeComponent implements OnInit {
   // inputs
   @Input() topicId: string;
   @Input() opinionId: string;
-  @Input() userId: string;
+
+  // variables
+  userId: string;
 
   // data models
   numOfLikes: number;
-  isLiked: boolean;
+  hasLiked: boolean;
   isEnabled: boolean;
 
   constructor(
-    opinionsService: OpinionsService
-  ) {}
+    private opinionsService: OpinionsService,
+    private authService: AuthService
+  ) {
+    this.isEnabled = true;
+  }
 
+  ngOnInit(): void {
+    this.userId = this.authService.retrieveUser()._id;
 
+    // retrieve the opinion
+    this.opinionsService.getOpinion(this.topicId, this.opinionId)
+      .subscribe((res) => {
+        // set number of likes
+        this.numOfLikes = res.likes.number;
+
+        // check if the user has disliked
+        if (res.dislikes.users.indexOf(this.userId) > -1) {
+          this.isEnabled = false;
+        }
+
+        // check if user has liked already
+        if (res.likes.users.indexOf(this.userId) > -1) {
+          this.hasLiked = true;
+        }
+      });
+  }
+
+  like(): void {
+
+  }
+
+  unlike(): void {
+
+  }
 }
