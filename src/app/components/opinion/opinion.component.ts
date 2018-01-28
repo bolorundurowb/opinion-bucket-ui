fc20cb1b4518d5f3615d23def2011d396360f527
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-28 23:09:47 +0100

    #40 | fix a syntax error

--- src/app/components/opinion/opinion.component.ts
+++ src/app/components/opinion/opinion.component.ts
@@ -7,10 +7,10 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class OpinionComponent {
   // inputs
-  @Input opinion: any;
+  @Input() opinion: any;
 
   // outputs
-  @Output error = new EventEmitter<any>();
+  @Output() error = new EventEmitter<any>();
 
   constructor() {
   }
