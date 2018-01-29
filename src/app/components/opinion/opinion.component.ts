93ef49ce0ea76b91075b7a5fe900b7887edb3f61
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-29 15:42:18 +0100

    #40 | add date display and neutral profile photo

--- src/app/components/opinion/opinion.component.ts
+++ src/app/components/opinion/opinion.component.ts
@@ -1,11 +1,12 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-opinion',
   templateUrl: './opinion.component.html',
   styleUrls: []
 })
-export class OpinionComponent {
+export class OpinionComponent implements OnInit {
   // inputs
   @Input() opinion: any;
 
@@ -14,8 +15,29 @@ export class OpinionComponent {
 
   // data models
   author: any;
+  creationTime: string;
+  displayName: string;
+  displayPhoto: string;
 
   constructor() {
+    // set the opinion creation date as relative time
+    if (this.opinion.date) {
+      this.creationTime = moment(this.opinion.date, 'YYYY-MM-DDTHH:mm:ss.sssZ').fromNow();
+    }
+
+    // show the username or full name as determined
+    if (this.opinion.showName) {
+      this.displayName = `${this.opinion.firstName} ${this.opinion.lastName}`;
+    } else {
+      this.displayName = this.opinion.username;
+    }
+
+    // set a neutral profile photo until one is loaded
+    this.displayPhoto = './../../../assets/avatars/decline.jpg';
+  }
+
+  ngOnInit(): void {
+
   }
 
   propagateError(err: any): void {
