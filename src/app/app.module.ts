36cf920e5bb5037c2e536547d5a450e3679767b4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-01-31 09:55:47 +0100

    #48 | add the components to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 // modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {SuiModule} from 'ng2-semantic-ui';
-
 // components
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {TopicListComponent} from './pages/topic-list/topic-list.component';
 import {RouterModule, Routes} from '@angular/router';
 import {TopicDetailsComponent} from './pages/topic-details/topic-details.component';
+import {OpinionsComponent} from './components/opinions/opinions.component';
+import {OpinionComponent} from './components/opinion/opinion.component';
+import {LikeComponent} from './components/like/like.component';
+import {DislikeComponent} from './components/dislike/dislike.component';
 
 const appRoutes: Routes = [
   {
@@ -28,7 +31,11 @@ const appRoutes: Routes = [
     AppComponent,
     NavbarComponent,
     TopicListComponent,
-    TopicDetailsComponent
+    TopicDetailsComponent,
+    OpinionsComponent,
+    OpinionComponent,
+    LikeComponent,
+    DislikeComponent
   ],
   imports: [
     BrowserModule,
